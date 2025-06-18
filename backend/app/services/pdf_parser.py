4a306a284cb85d700747161664ebafea90499f61
Ruslan <devoratrixanimus@gmail.com>
2025-06-19 00:04:22 +0500

    Add OCR Language to Dockerfile

--- backend/app/services/pdf_parser.py
+++ backend/app/services/pdf_parser.py
@@ -5,6 +5,6 @@ def extract_text_from_pdf(images: List):
     full_text = ""
     config = "--oem 3 --psm 6"  # оптимальный режим
     for img in images[:2]:  # максимум 2 страницы
-        text = pytesseract.image_to_string(img, lang="rus+eng", config=config)
+        text = pytesseract.image_to_string(img, lang="kaz+rus+eng", config=config)
         full_text += text + "\n"
     return full_text.strip()
